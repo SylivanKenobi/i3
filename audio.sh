6cdcb78aba134e930f8206a7927e633b2afd31a3
Sylivan Kenobi <sypsilon@hotmail.com>
2021-11-20 21:09:30 +0100

    improve audio independent of index

--- audio.sh
+++ audio.sh
@@ -15,6 +15,19 @@ case "${1:-}" in
       xargs -r -I{} pacmd move-sink-input {} $1 ||
         echo failed
     ;;
+  ("speackers"|"headset")
+    declare -A devices
+    devices=(["speackers"]="hdmi-stereo-extra4" ["headset"]="USB_Audio_Device-00")
+    echo switching default
+    sink_index=$(pacmd list-sinks | grep -B1 "${devices[$1]}" | awk '/index:/{print $NF}')
+    pacmd set-default-sink $sink_index ||
+      echo failed
+    echo switching applications
+    pacmd list-sink-inputs |
+      awk '/index:/{print $NF}' |
+      xargs -r -I{} pacmd move-sink-input {} $sink_index ||
+        echo failed
+    ;;
   (*)
     echo "Usage: $0 [|list|<sink name to switch to>]"
     ;;
